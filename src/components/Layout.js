5f5cf533f55c4815e45659e987d9042a9b942ca7
Adrian <adrian.william.jones@gmail.com>
2020-06-11 12:41:10 -0400

    Cleaned some styling, added grid header component, fixed success bug

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,64 +1,66 @@
-import React from "react";
+import React from 'react';
 
-import gameData from "../resources/gamedata.json";
-import "../styles/Layout.scss";
-import WordGrid from "./WordGrid";
+import gameData from '../resources/gamedata.json';
+import '../styles/Layout.scss';
+import WordGrid from './WordGrid';
 
 export default class Layout extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      allGridsComplete: false,
-      currentWordIndex: 0,
-      wordObjects: []
-    };
+		this.state = {
+			allGridsComplete: false,
+			currentWordIndex: 0,
+			wordObjects: []
+		};
 
-    this.onGridComplete = this.onGridComplete.bind(this);
-    this.requestWordData = this.requestWordData.bind(this);
-  }
+		this.onGridComplete = this.onGridComplete.bind(this);
+		this.requestWordData = this.requestWordData.bind(this);
+	}
 
-  componentDidMount() {
-    this.requestWordData();
-  }
+	componentDidMount() {
+		this.requestWordData();
+	}
 
-  onGridComplete() {
-    let { allGridsComplete, currentWordIndex, wordObjects } = this.state;
+	onGridComplete() {
+		let { allGridsComplete, currentWordIndex, wordObjects } = this.state;
 
-    if (currentWordIndex + 1 < wordObjects.length) {
-      currentWordIndex += 1;
-    } else {
-      allGridsComplete = true;
-    }
+		if (currentWordIndex + 1 < wordObjects.length) {
+			currentWordIndex += 1;
+		} else {
+			allGridsComplete = true;
+		}
 
-    this.setState({ allGridsComplete, currentWordIndex });
-  }
+		this.setState({ allGridsComplete, currentWordIndex });
+	}
 
-  requestWordData() {
-    const { wordObjects } = this.state;
-    gameData.forEach(wordObject => wordObjects.push(wordObject));
+	requestWordData() {
+		let { wordObjects } = this.state;
+		gameData.forEach((wordObject) => wordObjects.push(wordObject));
 
-    this.setState({
-      wordObjects: wordObjects
-    });
-  }
+		this.setState({
+			wordObjects: wordObjects
+		});
+	}
 
-  render() {
-    const { allGridsComplete, wordObjects, currentWordIndex } = this.state;
+	render() {
+		const { allGridsComplete, wordObjects, currentWordIndex } = this.state;
 
-    return (
-      <div className="layout">
-        <h2>Let's Translate!</h2>
-        {!allGridsComplete && (
-          <WordGrid
-            currentWordData={wordObjects[currentWordIndex]}
-            onGridComplete={this.onGridComplete}
-          />
-        )}
-        {allGridsComplete && (
-          <h2 className="finished-text">Congratulations! You're an expert linguist.</h2>
-        )}
-      </div>
-    );
-  }
+		return (
+			<div className="layout">
+				<h2>Let's Translate!</h2>
+				{!allGridsComplete && (
+					<WordGrid
+						currentWordData={wordObjects[currentWordIndex]}
+						onGridComplete={this.onGridComplete}
+					/>
+				)}
+				{allGridsComplete && (
+					<h2 className="finished-text">
+						Congratulations! You're an expert linguist.
+					</h2>
+				)}
+			</div>
+		);
+	}
 }
--- src/components/WordGrid.js
+++ src/components/WordGrid.js
@@ -1,320 +1,342 @@
-import React from "react";
-import "../styles/WordGrid.scss";
+import React from 'react';
+import GridHeader from './GridHeader';
+import '../styles/WordGrid.scss';
 
 const languages = {
-  es: "Spanish"
+	es: 'Spanish',
 };
 
 export default class WordGrid extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      currentWordData: null,
-      characterGrid: null,
-      gridComplete: false,
-      successfulFinds: [],
-      selectedCharacterCoords: {},
-      startPoint: null,
-      wordLocations: {}
-    };
+		this.state = {
+			currentWordData: null,
+			characterGrid: null,
+			gridComplete: false,
+			successfulFinds: [],
+			selectedCharacterCoords: {},
+			startPoint: null,
+			wordLocations: {}
+		};
 
-    this.buildCharacterGrid = this.buildCharacterGrid.bind(this);
-    this.onCellClick = this.onCellClick.bind(this);
-    this.onCellMove = this.onCellMove.bind(this);
-    this.onCellRelease = this.onCellRelease.bind(this);
-    this.onGridComplete = this.onGridComplete.bind(this);
-    this.onSuccessfulFind = this.onSuccessfulFind.bind(this);
-  }
+		this.buildCharacterGrid = this.buildCharacterGrid.bind(this);
+		this.onCellClick = this.onCellClick.bind(this);
+		this.onCellMove = this.onCellMove.bind(this);
+		this.onCellRelease = this.onCellRelease.bind(this);
+		this.onGridComplete = this.onGridComplete.bind(this);
+		this.onSuccessfulFind = this.onSuccessfulFind.bind(this);
+	}
 
-  componentDidUpdate(prevProps) {
-    if (this.props.currentWordData !== prevProps.currentWordData) {
-      this.setState(
-        {
-          currentWordData: this.props.currentWordData,
-          wordLocations: this.props.currentWordData.word_locations
-        },
-        () => this.buildCharacterGrid()
-      );
-    }
-  }
+	componentDidUpdate(prevProps) {
+		if (this.props.currentWordData !== prevProps.currentWordData) {
+			const wordLocations = this.props.currentWordData.word_locations,
+				totalWordsToFind = Object.getOwnPropertyNames(wordLocations).length;
+			this.setState(
+				{
+					currentWordData: this.props.currentWordData,
+					totalWordsToFind,
+					wordLocations,
+				},
+				() => this.buildCharacterGrid()
+			);
+		}
+	}
 
-  buildCharacterGrid() {
-    const { currentWordData } = this.state;
+	buildCharacterGrid() {
+		const { currentWordData } = this.state;
 
-    if (currentWordData !== undefined) {
-      let characterGrid = currentWordData.character_grid.map((row, index) => {
-        let yPosition = index;
+		if (currentWordData !== undefined) {
+			let characterGrid = currentWordData.character_grid.map((row, index) => {
+				let yPosition = index;
 
-        // create a table row for each row of characters
-        return (
-          <tr className="grid-row" key={index}>
-            {row.map((character, index) => {
-              // create a cell for each character in the row
-              const cellPosition = { x: index, y: yPosition };
-              let classnames = this.determineCellStyle(cellPosition);
+				// create a table row for each row of characters
+				return (
+					<tr className="grid-row" key={index}>
+						{row.map((character, index) => {
+							// create a cell for each character in the row
+							const cellPosition = { x: index, y: yPosition };
+							let classnames = this.determineCellStyle(cellPosition);
 
-              return (
-                <td
-                  className={classnames}
-                  key={index}
-                  x-value={index}
-                  onMouseDown={() => this.onCellClick(cellPosition)}
-                  onMouseUp={this.onCellRelease}
-                  onMouseOver={() => this.onCellMove(cellPosition)}
-                >
-                  <div className="character-container">
-                    {character.toUpperCase()}
-                  </div>
-                </td>
-              );
-            })}
-          </tr>
-        );
-      });
-      this.setState({ characterGrid });
-    }
-  }
+							return (
+								<td
+									className={classnames}
+									key={index}
+									x-value={index}
+									onMouseDown={() => this.onCellClick(cellPosition)}
+									onMouseUp={this.onCellRelease}
+									onMouseOver={() => this.onCellMove(cellPosition)}
+								>
+									<div className="character-container">
+										{character.toUpperCase()}
+									</div>
+								</td>
+							);
+						})}
+					</tr>
+				);
+			});
+			this.setState({ characterGrid });
+		}
+	}
 
-  determineCellStyle(position) {
-    const { selectedCharacterCoords, startPoint, successfulFinds } = this.state;
-    let classnames = "grid-cell";
+	/* Ensure we don't add duplicate objects to succesfulFinds array */
+	containsDuplicateEntry(successfulFinds, locationString) {
+		let containsDuplicate = false;
 
-    // Successful finds array holds objects that contain a character lookup and
-    // a string created from those coordinates
-    if (successfulFinds.length > 0) {
-      successfulFinds.forEach(find => {
-        const characterDictionary = find.selectedCharacterCoords;
-        if (
-          characterDictionary[position.y] &&
-          characterDictionary[position.y][position.x]
-        ) {
-          classnames += " complete";
-        }
-      });
-    }
+		successfulFinds.forEach((object) => {
+			if (object.locationString === locationString) {
+				containsDuplicate = true;
+			}
+		});
 
-    // Highlighted class is applied only if points have been selected
-    if (
-      startPoint !== null &&
-      selectedCharacterCoords[position.y] &&
-      selectedCharacterCoords[position.y][position.x]
-    ) {
-      classnames += " highlighted";
-    }
+		return containsDuplicate;
+	}
 
-    return classnames;
-  }
+	determineCellStyle(position) {
+		const { selectedCharacterCoords, startPoint, successfulFinds } = this.state;
+		let classnames = 'grid-cell';
 
-  determineDiagonalSelections(startPoint, endPoint, selectedCharacterCoords) {
-    const yDiff = endPoint.y - startPoint.y,
-      xDiff = endPoint.x - startPoint.x;
-    let locationString = "";
+		// Successful finds array holds objects that contain a character lookup and
+		// a string created from those coordinates
+		if (successfulFinds.length > 0) {
+			successfulFinds.forEach((find) => {
+				const characterDictionary = find.selectedCharacterCoords;
+				if (
+					characterDictionary[position.y] &&
+					characterDictionary[position.y][position.x]
+				) {
+					classnames += ' complete';
+				}
+			});
+		}
 
-    // Diagonal points have equivalent differences between x & y values
-    if (Math.abs(yDiff) === Math.abs(xDiff)) {
-      const topValue = yDiff > 0 ? startPoint : endPoint,
-        valuesRightward = (yDiff < 0 && xDiff < 0) || (yDiff > 0 && xDiff > 0);
+		// Highlighted class is applied only if points have been selected
+		if (
+			startPoint !== null &&
+			selectedCharacterCoords[position.y] &&
+			selectedCharacterCoords[position.y][position.x]
+		) {
+			classnames += ' highlighted';
+		}
 
-      // Loop captures diagonal values starting at topmost cell
-      for (let i = 0; i <= Math.abs(yDiff); i++) {
-        const yValue = topValue.y + i,
-          xValue = valuesRightward ? topValue.x + i : topValue.x - i;
-        selectedCharacterCoords[yValue] = {};
-        selectedCharacterCoords[yValue][xValue] = true;
-        locationString +=
-          locationString === ""
-            ? `${xValue},${yValue}`
-            : `,${xValue},${yValue}`;
-      }
-    }
+		return classnames;
+	}
 
-    return locationString;
-  }
+	determineDiagonalSelections(startPoint, endPoint, selectedCharacterCoords) {
+		const yDiff = endPoint.y - startPoint.y,
+			xDiff = endPoint.x - startPoint.x;
+		let locationString = '';
 
-  determineHorizontalSelections(startPoint, endPoint, selectedCharacterCoords) {
-    const leftValue = startPoint.x < endPoint.x ? startPoint.x : endPoint.x,
-      rightValue = leftValue === startPoint.x ? endPoint.x : startPoint.x;
-    let locationString = "";
-    selectedCharacterCoords[endPoint.y] = {};
+		// Diagonal points have equivalent differences between x & y values
+		if (Math.abs(yDiff) === Math.abs(xDiff)) {
+			const topValue = yDiff > 0 ? startPoint : endPoint,
+				valuesRightward = (yDiff < 0 && xDiff < 0) || (yDiff > 0 && xDiff > 0);
 
-    for (let i = leftValue; i <= rightValue; i++) {
-      selectedCharacterCoords[endPoint.y][i] = true;
-      locationString +=
-        locationString === "" ? `${i},${endPoint.y}` : `,${i},${endPoint.y}`; // Xn,Yn
-    }
+			// Loop captures diagonal values starting at topmost cell
+			for (let i = 0; i <= Math.abs(yDiff); i++) {
+				const yValue = topValue.y + i,
+					xValue = valuesRightward ? topValue.x + i : topValue.x - i;
+				selectedCharacterCoords[yValue] = {};
+				selectedCharacterCoords[yValue][xValue] = true;
+				locationString +=
+					locationString === ''
+						? `${xValue},${yValue}`
+						: `,${xValue},${yValue}`;
+			}
+		}
 
-    return locationString;
-  }
+		return locationString;
+	}
 
-  determineVerticalSelections(startPoint, endPoint, selectedCharacterCoords) {
-    const topValue = startPoint.y < endPoint.y ? startPoint.y : endPoint.y,
-      bottomValue = topValue === startPoint.y ? endPoint.y : startPoint.y;
-    let locationString = "";
+	determineHorizontalSelections(startPoint, endPoint, selectedCharacterCoords) {
+		const leftValue = startPoint.x < endPoint.x ? startPoint.x : endPoint.x,
+			rightValue = leftValue === startPoint.x ? endPoint.x : startPoint.x;
+		let locationString = '';
+		selectedCharacterCoords[endPoint.y] = {};
 
-    for (let i = topValue; i <= bottomValue; i++) {
-      selectedCharacterCoords[i] = {};
-      selectedCharacterCoords[i][endPoint.x] = true;
-      locationString +=
-        locationString === "" ? `${endPoint.x},${i}` : `,${endPoint.x},${i}`; // Xn,Yn
-    }
+		for (let i = leftValue; i <= rightValue; i++) {
+			selectedCharacterCoords[endPoint.y][i] = true;
+			locationString +=
+				locationString === '' ? `${i},${endPoint.y}` : `,${i},${endPoint.y}`; // Xn,Yn
+		}
 
-    return locationString;
-  }
+		return locationString;
+	}
 
-  onCellClick(position) {
-    // When a cell has been clicked highlighting should
-    // occur until release
-    let { selectedCharacterCoords } = this.state;
-    selectedCharacterCoords[position.y] = {};
-    selectedCharacterCoords[position.y][position.x] = true;
+	determineVerticalSelections(startPoint, endPoint, selectedCharacterCoords) {
+		const topValue = startPoint.y < endPoint.y ? startPoint.y : endPoint.y,
+			bottomValue = topValue === startPoint.y ? endPoint.y : startPoint.y;
+		let locationString = '';
 
-    this.setState(
-      {
-        selectedCharacterCoords,
-        startPoint: position
-      },
-      () => this.buildCharacterGrid()
-    );
-  }
+		for (let i = topValue; i <= bottomValue; i++) {
+			selectedCharacterCoords[i] = {};
+			selectedCharacterCoords[i][endPoint.x] = true;
+			locationString +=
+				locationString === '' ? `${endPoint.x},${i}` : `,${endPoint.x},${i}`; // Xn,Yn
+		}
 
-  onCellMove(endPoint) {
-    const { startPoint } = this.state;
-    let { gridComplete, wordLocations } = this.state,
-      locationString = "";
+		return locationString;
+	}
 
-    // Only store new selected character coordinates if a starting point has been recorded
-    if (startPoint !== null) {
-      let selectedCharacterCoords = {};
+	onCellClick(position) {
+		// When a cell has been clicked highlighting should
+		// occur until release
+		let { selectedCharacterCoords } = this.state;
+		selectedCharacterCoords[position.y] = {};
+		selectedCharacterCoords[position.y][position.x] = true;
 
-      // Equivalent y values, end point is horizontal
-      if (startPoint.y === endPoint.y) {
-        locationString = this.determineHorizontalSelections(
-          startPoint,
-          endPoint,
-          selectedCharacterCoords
-        );
-      }
-      // Equivalent x values, end point is vertical
-      else if (startPoint.x === endPoint.x) {
-        locationString = this.determineVerticalSelections(
-          startPoint,
-          endPoint,
-          selectedCharacterCoords
-        );
-      } else {
-        locationString = this.determineDiagonalSelections(
-          startPoint,
-          endPoint,
-          selectedCharacterCoords
-        );
-      }
+		this.setState(
+			{
+				selectedCharacterCoords,
+				startPoint: position,
+			},
+			() => this.buildCharacterGrid()
+		);
+	}
 
-      if (wordLocations[locationString] && !gridComplete) {
-        this.onSuccessfulFind(selectedCharacterCoords, locationString);
-      } else {
-        this.setState(
-          {
-            selectedCharacterCoords
-          },
-          () => this.buildCharacterGrid()
-        );
-      }
-    }
-  }
+	onCellMove(endPoint) {
+		const { startPoint } = this.state;
+		let { gridComplete, wordLocations } = this.state,
+			locationString = '';
 
-  onCellRelease() {
-    // On mouse up, highlighting is stopped and current
-    // selection is cleared
-    this.setState(
-      {
-        selectedCharacterCoords: {},
-        startPoint: null
-      },
-      () => this.buildCharacterGrid()
-    );
-  }
+		// Only store new selected character coordinates if a starting point has been recorded
+		if (startPoint !== null) {
+			let selectedCharacterCoords = {};
 
-  onGridComplete() {
-    this.setState(
-      {
-        gridComplete: false,
-        successfulFinds: []
-      },
-      this.props.onGridComplete()
-    );
-  }
+			// Equivalent y values, end point is horizontal
+			if (startPoint.y === endPoint.y) {
+				locationString = this.determineHorizontalSelections(
+					startPoint,
+					endPoint,
+					selectedCharacterCoords
+				);
+			}
+			// Equivalent x values, end point is vertical
+			else if (startPoint.x === endPoint.x) {
+				locationString = this.determineVerticalSelections(
+					startPoint,
+					endPoint,
+					selectedCharacterCoords
+				);
+			} else {
+				locationString = this.determineDiagonalSelections(
+					startPoint,
+					endPoint,
+					selectedCharacterCoords
+				);
+			}
 
-  onSuccessfulFind(selectedCharacterCoords, locationString) {
-    let { successfulFinds, wordLocations } = this.state,
-      gridComplete = false;
-    const wordLocationStrings = Object.getOwnPropertyNames(wordLocations);
+			if (wordLocations[locationString] && !gridComplete) {
+				this.onSuccessfulFind(selectedCharacterCoords, locationString);
+			} else {
+				this.setState(
+					{
+						selectedCharacterCoords,
+					},
+					() => this.buildCharacterGrid()
+				);
+			}
+		}
+	}
 
-    // Storing selectedCharacterCoords to do character comparison when
-    // building the grid and corresponding location string to determine grid completion
-    successfulFinds.push({
-      selectedCharacterCoords,
-      locationString
-    });
+	/* Clear highlighting and selection on mouse up */
+	onCellRelease() {
+		this.setState(
+			{
+				selectedCharacterCoords: {},
+				startPoint: null
+			},
+			() => this.buildCharacterGrid()
+		);
+	}
 
-    const foundLocationStrings = successfulFinds.map(
-      find => find.locationString
-    );
+	onGridComplete() {
+		this.setState(
+			{
+				gridComplete: false,
+				successfulFinds: []
+			},
+			this.props.onGridComplete()
+		);
+	}
 
-    // Grid is solved if every location string in word locations has been found
-    if (successfulFinds.length >= wordLocationStrings.length) {
-      gridComplete = true;
+	onSuccessfulFind(selectedCharacterCoords, locationString) {
+		let { successfulFinds, wordLocations } = this.state,
+			gridComplete = false;
+		const wordLocationStrings = Object.getOwnPropertyNames(wordLocations);
 
-      for (let string of wordLocationStrings) {
-        if (!foundLocationStrings.includes(string)) {
-          gridComplete = false;
-        }
-      }
-    }
+		// Storing selectedCharacterCoords to do character comparison when
+		// building the grid and corresponding location string to determine grid completion
+		if (!this.containsDuplicateEntry(successfulFinds, locationString)) {
+			successfulFinds.push({
+				selectedCharacterCoords,
+				locationString
+			});
+		}
 
-    this.setState({ gridComplete, successfulFinds }, () =>
-      this.buildCharacterGrid()
-    );
-  }
+		const foundLocationStrings = successfulFinds.map(
+			(find) => find.locationString
+		);
 
-  render() {
-    const { characterGrid, currentWordData, gridComplete } = this.state;
+		// Grid is solved if every location string in word locations has been found
+		if (successfulFinds.length >= wordLocationStrings.length) {
+			gridComplete = true;
 
-    return (
-      <div className="grid-section">
-        {currentWordData && (
-          <div className="grid-heading">
-            <span>
-              Target Word:{" "}
-              <strong className="target-word">{currentWordData.word}</strong>
-            </span>
-            <br />
-            <span>
-              Find all{" "}
-              <strong>{languages[currentWordData.target_language]}</strong>{" "}
-              translations
-            </span>
-          </div>
-        )}
-        {characterGrid && (
-          <table className="character-grid">
-            <tbody>{characterGrid}</tbody>
-          </table>
-        )}
-        {gridComplete && (
-          <div className="button-area">
-            <span role="img" aria-label="party-popper">
-              &#x1F389;
-            </span>
-            <button className="continue-button" onClick={this.onGridComplete}>
-              Next
-            </button>
-            <span role="img" aria-label="party-popper">
-              &#x1F389;
-            </span>
-          </div>
-        )}
-      </div>
-    );
-  }
+			for (let string of wordLocationStrings) {
+				if (!foundLocationStrings.includes(string)) {
+					gridComplete = false;
+				}
+			}
+		}
+
+		this.setState({ gridComplete, successfulFinds }, () =>
+			this.buildCharacterGrid()
+		);
+	}
+
+	render() {
+		const {
+			characterGrid,
+			currentWordData,
+			gridComplete,
+			successfulFinds,
+			totalWordsToFind
+		} = this.state;
+
+		return (
+			<div className="grid-section">
+				{currentWordData && (
+					<GridHeader
+						currentWord={currentWordData.word}
+						foundWords={successfulFinds.length}
+						totalWordsToFind={totalWordsToFind}
+						targetLanguage={languages[currentWordData.target_language]}
+					/>
+				)}
+
+				{characterGrid && (
+					<div className="grid-container">
+						<table className="character-grid">
+							<tbody>{characterGrid}</tbody>
+						</table>
+					</div>
+				)}
+
+				{gridComplete && (
+					<div className="button-area">
+						<span role="img" aria-label="party-popper">
+							&#x1F389;
+						</span>
+						<button className="continue-button" onClick={this.onGridComplete}>
+							Next
+						</button>
+						<span role="img" aria-label="party-popper">
+							&#x1F389;
+						</span>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
