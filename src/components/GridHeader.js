f790c6b87776656be27d5aa0066d7dd92ce4a41e
Adrian <adrian.william.jones@gmail.com>
2020-06-27 17:16:50 -0400

    Made grid header function component

--- src/components/GridHeader.js
+++ src/components/GridHeader.js
@@ -1,33 +1,25 @@
 import React from 'react';
 import '../styles/GridHeader.scss';
 
-export default class GridHeader extends React.Component {
-	render() {
-		const {
-			currentWord,
-			foundWords,
-			targetLanguage,
-			totalWordsToFind,
-		} = this.props;
-
-		return (
-			<div className="grid-header">
-				<div className="instructions">
-					<span>
-						Find all <strong>{targetLanguage}</strong> translations
-					</span>
-					<br />
-					<br />
-					<span>
-						Target Word: <strong className="target-word">{currentWord}</strong>
-					</span>
-				</div>
-				<div className="progress">
-					<span>
-						{foundWords}/{totalWordsToFind} words found
-					</span>
-				</div>
+export default function GridHeader(props) {
+	return (
+		<div className="grid-header">
+			<div className="instructions">
+				<span>
+					Find all <strong>{props.targetLanguage}</strong> translations
+				</span>
+				<br />
+				<br />
+				<span>
+					Target Word:{' '}
+					<strong className="target-word">{props.currentWord}</strong>
+				</span>
 			</div>
-		);
-	}
+			<div className="progress">
+				<span>
+					{props.foundWords}/{props.totalWordsToFind} words found
+				</span>
+			</div>
+		</div>
+	);
 }
